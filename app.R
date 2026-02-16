442a21956db5eae3e0c970b7dd57e43d327a5bdc
Leland-Russell <172640492+Leland-Russell@users.noreply.github.com>
2026-02-16 09:53:39 -0800

    good app, try mobile view at https://mobileviewer.io/

--- app.R
+++ app.R
@@ -1,5 +1,47 @@
-library(tidyr); library(dplyr)
-library(shiny); library(htmltools); library(bslib) # last two libs might not be necessary
+library(tidyr); library(dplyr) # these are good to have, but might not be necessary. This is mostly base R or Shiny
+library(shiny); library(htmltools); library(bslib) # shiny libraries
+
+
+
+submit_form = function(input, output, extension) {
+  return(isolate({
+    # I'm using an isolate block to "isolate" the function from the reactive input fields I use here. This is because otherwise any change in an input would re-evaluate this and  possibly duplicate the data many times.
+#    if (input$geolocation) {
+    truth = TRUE
+    truth = truth && (input[[paste0("observer_", extension)]] != "")
+
+    for (var in c("nest_state_",
+		  "parents_",
+		  "bag_construction_",
+		  "predation_",
+		  "stage_")) {
+      truth = truth && (input[[paste0(var, extension)]] != -1)
+    }
+
+    if (truth) { 
+      # if there is geolocation and all fields are filled out
+      data_string = paste(
+	input[[paste0("observer_", extension)]],
+        input[[paste0("parents_", extension)]],
+	input[[paste0("tags_", extension)]],
+        input[[paste0("predation_", extension)]],
+	input[[paste0("stage_", extension)]],
+        input[[paste0("bag_", extension)]],
+        format(Sys.time(), "%x"),  # get the date
+        format(Sys.time(), "%R:%S:%p"), # get the time
+	input$lat, # latitude and longitude from the widget we force on page load
+        input$long,
+        sep = ",") # sep should be a comma for csv format
+      data_string = gsub("\'", "\\\\\'", data_string) # escape single and double quotes
+      data_string = gsub("\"", "\\\\\'", data_string) 
+      system(paste0("echo ", data_string, " >> ", extension, ".csv")) # save into a CSV
+      output[[paste0("submit_form_", extension, "_result")]] = renderText("Saved data")
+    } else {
+      output[[paste0("submit_form_", extension, "_result")]] = renderText("Please fill out all fields")
+    } 
+  }))
+}
+
 
 nest_state_options = list(
   "Select a value" = -1, # default value
@@ -40,11 +82,13 @@ stage_options = list(
 
 ui = page_navbar(
   title = "Bushtit Database Entry",
-  sidebar = sidebar(
-    tags$script(
-      paste(system("cat get_location.js", intern = TRUE), collapse="\n")) # query location
-  ),
+#  sidebar = sidebar(
+#    tags$script(
+#      paste(system("cat get_location.js", intern = TRUE), collapse="\n")) # query location
+#  ),
   nav_panel(
+    tags$script(
+      paste(system("cat get_location.js", intern = TRUE), collapse="\n")), # query location
     style="margin-bottom:50px;",
     title = "Nest Update",
     textInput("observer_update_form", "Observer", value = ""), # could be a choice if there are a discrete number of people... would also require more maintenance
@@ -56,15 +100,15 @@ ui = page_navbar(
     textInput("tags_update_form", "Tags?", "None"), # I don't know what type of data tags_update_form are. This may be better suited to a selectInput()
     selectInput("bag_construction_update_form", "Bag Construction Types", bag_construction_options),
     selectInput("predation_update_form", "Signs of Predation?", predation_options),
-    selectInput("stage_udpate_form", "Stage", stage_options), # Is this the same as nest state? If so, we don't need it and we can just derive it
-    actionButton("submit_form_update_nest", "Submit Nest Update", width = 300),
-    textOutput("submit_form_update_nest_result"),
+    selectInput("stage_update_form", "Stage", stage_options), # Is this the same as nest state? If so, we don't need it and we can just derive it
+    actionButton("submit_form_update_form", "Submit Nest Update", width = 300),
+    textOutput("submit_form_update_form_result"),
     headerPanel(""), headerPanel("") # so we have extra white space at the end of the page
   ),
   nav_panel(
     style="margin-bottom:50px;",
     title = "New Nest",
-    textInput("observer", "Observer", value = ""), # could be a choice if there are a discrete number of people... would also require more maintenance
+    textInput("observer_new_nest", "Observer", value = ""), # could be a choice if there are a discrete number of people... would also require more maintenance
     # R can get time for free
     # we can get gps for free
     # We can get date for free
@@ -78,54 +122,24 @@ ui = page_navbar(
     textOutput("submit_form_new_nest_result"),
     headerPanel(""), headerPanel("") # so there's extra white space
   ),
-  nav_panel(
-    title = "Description"
+  nav_panel( # we can put a description here
+    title = "Description",
+    h1("Welcome to our shiny app."),
+    p("Hiii. This is a description. This one is temporary. This is a shiny app so people in the D'Amelio lab can submit update forms from their phone. In principle, this should be hosted on some server, and the csv files could be hooked up to a SQL database.")
   ),
   nav_menu( # looks bad on the shiny app right now, but will look better
     title = "Links",
     nav_item(tagList(a("GitHub Repo",href="https://github.com/data-at-reed-college/bushtit-database"))),
+    nav_item(tagList(a("D'Amelio lab?", href="https://www.reed.edu/faculty-profiles/profiles/damelio-pietro.html"))),
     nav_item("lab pages?")
   )
 )
 
 
 server = function(input, output) {
-  # I think that these output$* assignments aren't necessary
-#  output$observer = reactive({input$observer})
-#  output$parents_update_form = reactive({input$parents_update_form})
-#  output$tags_update_form = reactive({input$tags_update_form})
-#  output$predation = reactive({input$predation})
-#  output$stage = reactive({input$stage})
-
-  observeEvent(input$submit_form_update_nest, isolate({
-    # I'm using an isolate block to "isolate" the function from the reactive input fields I use here. This is because otherwise any change in an input would re-evaluate this and  possibly duplicate the data many times.
-#    if (input$geolocation) {
-      if ((input$observer_update_form != "") && (input$parents_update_form != -1) && (input$predation_update_form != -1) && (input$stage_udpate_form != -1) && (input$bag_construction_update_form != -1)) { 
-	# if there is geolocation and all fields are filled out
-        data_string = paste(
-	  input$observer_update_form, 
-          input$parents_update_form, 
-	  input$tags_update_form, 
-          input$predation_update_form, 
-	  input$stage_udpate_form, 
-	  input$bag_construction_update_form,
-          format(Sys.time(), "%x"), 
-          format(Sys.time(), "%R:%S:%p"),
-	  input$lat,
-	  input$long,
-          sep = ",")
-	data_string = gsub("\'", "\\\\\'", data_string)
-	data_string = gsub("\"", "\\\\\'", data_string) # escape single and double quotes
-	system(paste("echo", data_string, ">> nest_updates.csv")) # save into a CSV
-	output$submit_form_update_nest_result = renderText("Saved data")
-      } else {
-	output$submit_form_update_nest_result = renderText("Please fill out all fields")
-      } 
-#    } else {
-#	output$submit_form_update_nest_result = renderText("Unable to get location. Please allow location")
-#      }
-    })
-  )
+  # I defined a helper function at the beginning of the document to automate submitting the form based on which form we  are submitting (because they require the same information, but I wanted them to be distinct fields.
+  observeEvent(input$submit_form_update_form, submit_form(input, output, extension = "update_form"))
+  observeEvent(input$submit_form_new_nest, submit_form(input, output, extension = "new_nest"))
 }
 
 shinyApp(ui=ui, server=server)
